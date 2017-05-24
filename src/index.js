d8673c384f0f6c09f5431141cd9ee6aecc9771f0
BeijiYang <langyuyang@outlook.com>
2017-05-24 15:06:12 +0800

    Display the move locations in the format "(1, 3)" instead of "6".

--- src/index.js
+++ src/index.js
@@ -97,16 +97,42 @@ class Game extends React.Component {
       }],
       xIsNext: true,
       stepNumber: 0,
+      // historyLocation:[],
     };
   }
 
+  calculateWinner(squares) {
+    // const history=this.state.history;
+    // const current=history[this.state.stepNumber];
+    // const SPSPsquares=current.squares;
+
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+
   handleClick(i) {
     // const history = this.state.history;
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
+    const copyHistory = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = copyHistory[copyHistory.length - 1];
     const squares = current.squares.slice();
 
-    if (calculateWinner(squares) || squares[i]) {
+    if (this.calculateWinner(squares) || squares[i]) {
       return;
     }
     //ignore the click if someone has already won the game or if a square is already filled
@@ -114,10 +140,10 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? "X" : "O";
 
     this.setState({
-      history: history.concat([{
+      history: copyHistory.concat([{
         squares: squares
       }]),
-      stepNumber: history.length,
+      stepNumber: copyHistory.length,
       xIsNext: !this.state.xIsNext,
     });
   }
@@ -134,10 +160,11 @@ class Game extends React.Component {
     const history = this.state.history;
     // const current = history[history.length - 1];
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = this.calculateWinner(current.squares);
 
-    console.log(history);
-    console.log(current.squares);
+    // console.log(history.length);
+    // console.log(this.state.stepNumber);
+    // console.log("current.squares:"+current.squares);
 
     const moves = history.map((step,move) => {
       const desc = move ?
@@ -182,22 +209,27 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+//calculateWinner
+// function calculateWinner(squares) {
+//   // const history=this.state.history;
+//   // const lastHistory=history[this.state.stepNumber];
+//   // const squares=lastHistory.squares;
+//
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
