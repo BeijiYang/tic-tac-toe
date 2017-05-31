807855769f6f691018c6680aba9bb3b472fa5ab2
BeijiYang <langyuyang@outlook.com>
2017-05-31 11:54:03 +0800

    add AI

--- src/index.js
+++ src/index.js
@@ -187,9 +187,189 @@ class Game extends React.Component {
     });
   }
 
+  callAI(currentSquares) {
+    let aiMove = this.ai(currentSquares);
+    console.log(aiMove);
+    //按小格子的数字代号存储的历史
+    const copyHistory = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = copyHistory[copyHistory.length - 1];
+    const squares = current.squares.slice();
+
+    //按坐标系存储的历史。之所以下面这一句不需要跟上面一样“+ 1”，是因为第0步时，上一种记录方式的初始状态是[object]，对象里是一个数组[null,...,null]，而这种是空的[]。
+    const historyLocation = this.state.historyLocation.slice(0, this.state.stepNumber);
+    historyLocation.push(this.convert(aiMove));
+
+
+    //ignore the click if someone has already won the game or if a square is already filled
+    if (this.calculateWinner(squares)[0] || squares[aiMove]) {
+      return;
+    }
+
+    squares[aiMove] = this.state.xIsNext ? "X" : "O";
+
+    this.setState({
+      history: copyHistory.concat([{
+        squares: squares
+      }]),
+      stepNumber: copyHistory.length,
+      xIsNext: !this.state.xIsNext,
+      historyLocation: historyLocation,
+    });
+  }
+
+
+  ai(arr) {
+
+      var xIsNextqq = this.state.xIsNext;
+      var player= xIsNextqq ? 'X' : 'O';
+      var enemy = xIsNextqq ? 'O' : 'X';
+      var loacation=arr.map(function(item,index){
+          if(item===null){
+            // console.log(index);
+              return index;
+          }
+      }).filter(function(item){
+          return item!==undefined;
+      });
+      console.log(loacation);
+
+      var oCalc={};
+      // for(var i=0;i<arr.length;i++){
+          for(let j=0;j<loacation.length;j++){
+              var index=loacation[j];
+              switch(index){
+                  case 0:
+                  case 2:
+                  case 6:
+                  case 8:
+                    oCalc['loacation'+index]=judgeConner(index);
+                    break;
+                  case 1:
+                  case 3:
+                  case 5:
+                  case 7:
+                    oCalc['loacation'+index.toString()]=judgeSide(index);
+                    break;
+                  case 4:
+                    oCalc['loacation'+index.toString()]=judgeCenter(index);
+                    break;
+              }
+          }
+      // }
+
+        // var oCalc = { loacation3: 3000,
+        //               loacation5: 500,
+        //               loacation7: 50 }
+
+      console.log(JSON.stringify(oCalc));
+
+
+      function judgeConner(index) {
+        switch (index) {
+          case 0:
+          return conner(1,2,3,6,4,8);
+
+          case 2:
+          return conner(0,1,5,8,4,6);
+
+          case 6:
+          return conner(0,3,7,8,2,4);
+
+          case 8:
+          return conner(2,5,6,7,0,4);
+
+            break;
+          default:
+        }
+      }
+      function judgeSide(index) {
+        switch (index) {
+          case 1:
+          return side(4,7,0,2);
+
+          case 3:
+          return side(0,6,4,5);
+
+          case 5:
+          return side(2,8,3,4);
+
+          case 7:
+          return side(6,8,1,4);
+
+            break;
+          default:
+        }
+      }
+      function judgeCenter(index) {
+        return center(3,5,1,7,0,8,2,6)
+      }
+
+      function side(a,b,c,d) {
+        if ((arr[a]===player && arr[b]===player) || (arr[c]===player && arr[d]===player)) {
+          return 10000;
+        }else if((arr[a]===enemy && arr[b]===enemy) || (arr[c]===enemy && arr[d]===enemy)) {
+          return 1000;
+        }else if ((arr[a]===player || arr[b]===player) || (arr[c]===player || arr[d]===player)) {
+          return 100;
+        }else if ((arr[a]===enemy || arr[b]===enemy) || (arr[c]===enemy || arr[d]===enemy)) {
+          return -10;
+        }else {
+          return 0;
+        }
+      }
+
+      function conner(a,b,c,d,e,f) {
+        if ((arr[a]===player && arr[b]===player) || (arr[c]===player && arr[d]===player) || (arr[e]===player && arr[f]===player)) {
+          return 10000;
+        }else if((arr[a]===enemy && arr[b]===enemy) || (arr[c]===enemy && arr[d]===enemy)|| (arr[e]===enemy && arr[f]===enemy)) {
+          return 1000;
+        }else if ((arr[a]===player || arr[b]===player) || (arr[c]===player || arr[d]===player)|| (arr[e]===player || arr[f]===player)) {
+          return 100;
+        }else if ((arr[a]===enemy || arr[b]===enemy) || (arr[c]===enemy || arr[d]===enemy) || (arr[e]===enemy || arr[f]===enemy)) {
+          return -10;
+        }else {
+          return 0;
+        }
+      }
+
+      function center(a,b,c,d,e,f,g,h) {
+        if ((arr[a]===player && arr[b]===player) || (arr[c]===player && arr[d]===player) || (arr[e]===player && arr[f]===player) || (arr[g]===player && arr[h]===player)) {
+          return 10000;
+        }else if((arr[a]===enemy && arr[b]===enemy) || (arr[c]===enemy && arr[d]===enemy) || (arr[e]===enemy && arr[f]===enemy) || (arr[g]===enemy && arr[h]===enemy)) {
+          return 1000;
+        }else if ((arr[a]===player || arr[b]===player) || (arr[c]===player || arr[d]===player) || (arr[e]===player || arr[f]===player) || (arr[g]===player || arr[h]===player)) {
+          return 100;
+        }else if ((arr[a]===enemy || arr[b]===enemy) || (arr[c]===enemy || arr[d]===enemy) || (arr[e]===enemy || arr[f]===enemy) || (arr[g]===enemy || arr[h]===enemy)) {
+          return -10;
+        }else {
+          return 0;
+        }
+      }
+
+
+      const largest={
+          index:-1,
+          value:-100
+      };
+
+      for(var item in oCalc){
+          if(oCalc[item]>largest.value){
+              largest.index=parseInt(item.replace('loacation',''));
+              largest.value=oCalc[item];
+          }
+      }
+
+      console.log([player,JSON.stringify(largest)]);
+
+      return largest.index;
+    }
+
+
+  // ai(arr);
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
+    // console.log(this.ai(current.squares));
     const winner = this.calculateWinner(current.squares)[0];
     const winCase = this.calculateWinner(current.squares)[1];
     const coordinate = this.state.historyLocation.slice();
@@ -256,6 +436,7 @@ class Game extends React.Component {
           <div>{status}</div>
           <button onClick={() => this.toggleSortingOrder()}>reverse</button>
           <button onClick={() => this.reset()}>reset</button>
+          <button onClick={() => this.callAI(current.squares)}>AI</button>
           {
             this.state.ascendingOrder ?
             <ol>{moves}</ol> :
@@ -266,6 +447,31 @@ class Game extends React.Component {
     );
   }
 }
+
+
+/*
+只考虑一条线路上的情况
+线路上 有己方的二子连线，这手棋价值是10000。
+线路上 有对方的二子连线，这手棋价值可算作1000。
+线路上 只有己方一个子，价值算作100。
+线路上 只有对方的一个子,它的权重是-10。
+
+线路一个子都没有的情况，先手就定义为0吧——这里又有坑了。
+
+实际上的情况是，一手棋包含了2-4条线路。综合各条线路的判断下来。累加的权重就是这手棋的价值。
+
+
+角位是2,4,6,8。边位是1,3,5,7。中心位置为4。
+根据不同的情况返回不同的位置到对象oCalc中。（作为测试，可以把oCalc作为ai函数的return值）。
+
+角位，需要判定的有3条线路（横竖斜）
+边位，需要判断2条线路（横竖）
+中心位置需要判断4条线路：（横竖撇捺）
+接下来就是在各个函数里var一个value，一个个对arr进行判定。
+
+
+*/
+
 
 // ========================================
 
